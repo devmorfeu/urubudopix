fd20c648b6f572588532d38565cbcf2a9fe3701a
Gabriel Nogueira <gabriel.nogueira21@hotmail.com>
2023-07-23 04:11:52 -0300

    feat: add primeira versao do urubudopix em java e spring

--- urubudopix-java-spring/target/classes/target/classes/target/classes/db/migration/V1__create_table_user.sql
+++
@@ -1,8 +0,0 @@
-create table user_table(
-    id bigint not null auto_increment,
-    name varchar(50) not null,
-    primary key(id)
-);
-
-insert into user_table values(1, 'Malvadeza');
-insert into user_table values(2, 'Carimbo josé');
